73df75eaf87c8e92b168c566b09e8437312474d8
Evgeny Ivanov <busforward@gmail.com>
2017-10-26 14:49:09 +0500

    Прака каркаса - стили

--- js/function.js
+++ js/function.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
 
+	function setHeiHeight() {
+	    $('.crm__container').css({
+	        height: $(window).height() + 'px'
+	    });
+	}
+	setHeiHeight(); // устанавливаем высоту окна при первой загрузке страницы
+	$(window).resize( setHeiHeight ); // обновляем при изменении размеров окна
+
+
+	// Reset link whte attribute href="#"
+	$('[href*="#"]').click(function(event) {
+		event.preventDefault();
+	});
+
+
 	// Tabs toggle
 	$('.tab-menu__label').on('click', function(event) {
 		event.preventDefault();
