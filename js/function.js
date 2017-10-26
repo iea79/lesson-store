e118585718229996024b3c4b74a66c382375ecc1
Evgeny Ivanov <busforward@gmail.com>
2017-10-26 21:49:54 +0500

    Подправил адаптив

--- js/function.js
+++ js/function.js
@@ -1,11 +1,26 @@
+var TempApp = {
+    lgWidth: 1200,
+    mdWidth: 992,
+    smWidth: 768,
+    iOS: function() { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }
+};
+
+function isLgWidth() { return $(window).width() >= TempApp.lgWidth; } // >= 1200
+function isMdWidth() { return $(window).width() >= TempApp.mdWidth && $(window).width() < TempApp.lgWidth; } //  >= 992 && < 1200
+function isSmWidth() { return $(window).width() >= TempApp.smWidth && $(window).width() < TempApp.mdWidth; } // >= 768 && < 992
+function isXsWidth() { return $(window).width() < TempApp.smWidth; } // < 768
+function isIOS() { return TempApp.iOS(); } // for iPhone iPad iPod
+
 $(document).ready(function(){
 
 	function checkOnResize() {
+		var headerHeight = $('header').height();
+
 	    $('.crm__container').css({
-	        height: $(window).height() + 'px'
+	        height: $(window).height() - headerHeight
 	    });
 
-	    // Высота плиток
+	    // Высота плиток/grid
 	    $('[data-grid-match] > .grid__wrapper').each( function() {
     		gridItem = $(this).find('.grid__item');
 
